22c712b47f59fe09c129e2c09b0bd0f97dce59dd
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-20 01:41:43 +0100

    Adicionado o framework Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const { response } = require("express");
+const express = require("express")
+
+const app = express();
+
+app.get("/primeira-rota", (request, response) => {
+    return response.json({
+        message: "Acedeu à primeira rota",
+    });
+});
+
+app.listen(4002, () => console.log("Servidor está a correr na porta 4002"))
